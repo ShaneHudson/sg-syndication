f779152a9c1456e0706401c2bcde04a6813ea994
Shane Hudson <shanehudson@shanes-mbp.home>
2014-06-20 00:40:26 +0100

    Add the short of the post

--- sg-syndication-admin.php
+++ sg-syndication-admin.php
@@ -403,7 +403,7 @@ class sgSyndicationAdmin {
                         onClick="
                             jQuery(\'#' . $prefix . '_' . $service['name'] .'_ui\').toggle();
                             jQuery(\'#sg_syndication_tip\').show();
-                            jQuery(\'#' . $prefix . '_' . $service['name'] .'_text\').html( jQuery(\'#title\').val() + \'' . the_post().substring(0,134) . ' (' . get_the_ID() . ')\' );
+                            jQuery(\'#' . $prefix . '_' . $service['name'] .'_text\').html( jQuery(\'#title\').val() + \'' . substr(the_post(),0,134) . ' (' . get_the_ID() . ')\' );
                             jQuery(\'#' . $prefix . '_' . $service['name'] .'_counter span\').html(jQuery(\'#' . $prefix . '_' . $service['name'] .'_text\').val().length);
                         "
                     ';
